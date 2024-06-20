23ffac757b7df09d93794a76726c9dbc9679abad
NQD2308 <nqd230899@gmail.com>
2024-06-20 21:40:02 +0700

    search/find function with facade pattern

---
+++ FruitableShop/FruitableShop/Repository/ProductFacade.cs
@@ -0,0 +1,25 @@
+﻿using FruitableShop.Models;
+
+namespace FruitableShop.Repository
+{
+    // ========== Facade ========== //
+    public class ProductFacade
+    {
+        private readonly IRepository<Product> _productRepository;
+
+        public ProductFacade(IRepository<Product> productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public List<Product> SearchProducts(string keyword)
+        {
+            return _productRepository.SearchByName(keyword);
+        }
+
+        public Product Detail(int id)
+        {
+            return _productRepository.FindById(id);
+        }
+    }
+}
